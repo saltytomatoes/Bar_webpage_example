24ae30c64e61dcdf53d8c7483ff4b92be9fbf2c4
saltytomatoes <saltytomatoes1337@gmail.com>
2021-02-19 12:05:45 +0200

    sector2 description designed!

--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ const initialize = (function() {
     let mainImg = `<div class="parallax" data-parallax="scroll" data-z-index="1" data-image-src="/src/centeralpic.jpg"></div>`;
     
     let descriptionHeader = `<h1> Vitulis Place - Alcohol & Fun ! </h1>`;
-    let descriptionHolder = `<div class="subContainer"> ${descriptionHeader} </div>`;
-    let sector2 = `<div id="sector2"> ${descriptionHolder} </div>`;
+    let description = `<p> <span>The Vitulis Place Dance Bar</span> - The vitulis place is the first and the only dance bar in beer sheva wich combines
+                          new era, modern type of alcohol and music alongside with old fassioned, classic drinks and party spirit! in 2025 Our founder, Jack Vituli
+                          decided to bring a unique experience to his fellows in the negev and merge the past and the future in to the present!</p>`;
+    let descriptionHolder = `<div class="subContainer"> 
+                              ${descriptionHeader}
+                              ${description}
+                             </div>`;
+
+    let sector2 = `<div id="sector2">
+                    ${descriptionHolder}
+                   </div>`;
 
     
     Content.innerHTML = header +
