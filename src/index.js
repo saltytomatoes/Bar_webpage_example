378351b15597f488fbda769874924046527a74e2
saltytomatoes <saltytomatoes1337@gmail.com>
2021-02-19 22:50:29 +0200

    tabs visually designed, including lightbulbs logic

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const initialize = (function() {
     let header = `<header> Wellcome To Vitulis Place </header>`;
     let mainImg = `<div class="parallax" data-parallax="scroll" data-z-index="1" data-image-src="/src/pics/centeralpic.jpg"></div>`;
     
+
+    // sector 2 related
     let descriptionHeader = `<h1> Vitulis Place - Alcohol & Fun ! </h1>`;
     let description = `<p> <span>The Vitulis Place Dance Bar</span> - The vitulis place is the first and the only dance bar in beer sheva wich combines
                           new era, modern type of alcohol and music alongside with old fassioned, classic drinks and party spirit! in 2025 Our founder, Jack Vituli
@@ -25,7 +27,22 @@ const initialize = (function() {
 
     let mainImg2 = `<div class="parallax" data-parallax="scroll" data-z-index="1" data-image-src="/src/pics/centralpic2.jpg"></div>`;
 
-    let sector3 = `<div id="sector3"></div>`;
+
+    //sector 3 related
+
+    let btn1 = `<div class="button">Contact<div class="lightBulb"></div></div>`;
+    let btn2 = `<div class="button">Menu<div class="lightBulb"></div></div>`;
+    let btn3 = `<div class="button">Magic!<div class="lightBulb"></div></div>`;
+
+    let tabHolder = `<div id="tabHolder">
+                      ${btn1}
+                      ${btn2}
+                      ${btn3}
+                    </div>`;
+
+    let sector3 = `<div id="sector3">
+                    ${tabHolder}
+                  </div>`;
 
     
     Content.innerHTML = header +
@@ -34,3 +51,58 @@ const initialize = (function() {
                         mainImg2 +
                         sector3;
 })();
+
+
+const tabSetup = (function() {
+    
+    class Tab {
+        constructor(tabDiv){
+            this.divRef = tabDiv;
+            this.lightIsOn = false;
+            this.configEventListeners();
+        }
+
+        getDivRef () {
+            return this.divRef;
+        }
+
+
+        toggleOtherLights (skipCurrent) {
+            for(let i = 0; i < tabs.length; i++) {
+                if(skipCurrent == tabs[i])
+                    continue;
+                
+                if(tabs[i].lightIsOn) {
+                    tabs[i].toggleLight(false);
+                    tabs[i].lightIsOn = false;
+                }
+            }
+        }
+
+
+        toggleLight (clickFlag = true) {
+            let lightBulb = this.getDivRef().firstElementChild;
+            lightBulb.classList.toggle("on");
+            this.lightIsOn = true;
+
+            if(clickFlag)
+            this.toggleOtherLights(this);
+        }
+
+
+        configEventListeners() {
+            this.getDivRef().addEventListener("click",this.toggleLight.bind(this));
+        }
+    }
+
+    let tabs = Array.from(document.querySelectorAll(".button"));
+    tabs = tabs.map(tab => new Tab(tab));
+    
+    
+
+
+
+
+
+})();
+
